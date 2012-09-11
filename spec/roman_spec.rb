8a00052b287ec32c8741a59d9a5307398206d9bc
Cesar Bedoya <cesar.bedoyami@hotmail.com>
2012-09-11 02:47:56 -0500

    commit 8

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -2,34 +2,26 @@ require 'minitest/autorun'
 require_relative '../lib/roman'
 
 describe Roman do
-
-
-   it 'should convert 1 to I' do
-    roman = Roman.new #arrange
-	result = roman.convert(1) #act
-	result.must_equal 'I' #assert
+before do
+		@roman=Roman.new
+	end
+it 'should convert 1 to I' do
+     @roman.convert(1).must_equal "I"
   end
-  
-   it 'should convert 2 to II' do
-    roman = Roman.new #arrange
-	result = roman.convert 2 #act
-	result.must_equal 'II' #assert
+  it 'should convert 2 to II' do
+    @roman.convert(2).must_equal "II"
   end
-  
-   it 'should convert 3 to III' do
-    roman = Roman.new #arrange
-	result = roman.convert 3 #act
-	result.must_equal 'III' #assert
+  it 'should convert 3 to III' do
+    @roman.convert(3).must_equal "III"
   end
-    it 'should convert 4 to IV' do
-    roman = Roman.new #arrange
-	result = roman.convert 4 #act
-	result.must_equal 'IV' #assert
+  it 'should convert 4 to IV' do
+    @roman.convert(4).must_equal "IV"
+  end
+   it 'should convert 5 to V' do
+    @roman.convert(5).must_equal "V"
   end
    it 'should convert 6 to VI' do
-    roman = Roman.new #arrange
-	result = roman.convert 6 #act
-	result.must_equal 'VI' #assert
+    @roman.convert(6).must_equal "VI"
   end
      it 'should convert 7 to VII' do
     roman = Roman.new #arrange
