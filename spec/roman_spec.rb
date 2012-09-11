36a99f8b0728000353b3c0276497cfd61631d5e3
Cesar Bedoya <cesar.bedoyami@hotmail.com>
2012-09-11 02:20:17 -0500

    tercer test

--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -9,6 +9,17 @@ describe Roman do
 	result = roman.convert(1) #act
 	result.must_equal 'I' #assert
   end
-    
+  
+   it 'should convert 2 to II' do
+    roman = Roman.new #arrange
+	result = roman.convert 2 #act
+	result.must_equal 'II' #assert
+  end
+  
+   it 'should convert 3 to III' do
+    roman = Roman.new #arrange
+	result = roman.convert 3 #act
+	result.must_equal 'III' #assert
+  end
 
 end
